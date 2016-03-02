c5083e30f696365310fd975e20fe087fdc0c5a9b
Lucky Arte <vanhuten@Vanhuten.local>
2016-03-02 17:52:23 -0600

    Prototype 2 finished, but speech is very long, modifying speech

--- js/main.js
+++ js/main.js
@@ -1,38 +1,59 @@
+// START!
+	setTimeout(function(){
+		$('#agencyMails').focus();
+	},1000);
+// END START
+
 jQuery(document).ready(function($) {
 
-	$('#restartPage').click(function() {
-		location.reload();
-	});
+	
+	// RESTART
+		$('#restartPage').click(function() {
+			location.reload();
+		});
+	// END RESTART
 
-	$('.quotes').click(function() {
-		// quotesArray[0].remove();
-		$('.quotes:first-child').remove();
-		$('.quotes:first-child').removeClass('hiddenQuote');
-	});
+
+	// NORMAL FLOW
+		$('.quotes').click(function() {
+			// quotesArray[0].remove();
+			$('.quotes:first-child').remove();
+			$('.quotes:first-child').removeClass('hiddenQuote');
+		});
+	// END NORMAL FLOW 
+
+
 
 	// FIRST DECISION INTERACTION
-		$('#less20').click(function() {
-			$('.firstQuote').remove();
-			$('.quotes:first-child').removeClass('hiddenQuote');
-			$('#quote1').prepend('Meh, no son tantos.');
-		});
+		$(document).keyup(function(e) {
+	  		if (e.keyCode === 13 ) { 
+				// fingers(fingersInput);
+				var howManyMails = $('#agencyMails').val();
 
-		$('#yes20').click(function() {
-			$('.firstQuote').remove();
-			$('.quotes:first-child').removeClass('hiddenQuote');
-			$('#quote1').prepend('Si, son mas o menos los que yo te mandaría.');
-		});
 
-		$('#more20').click(function() {
-			$('.firstQuote').remove();
-			$('.quotes:first-child').removeClass('hiddenQuote');
-			$('#quote1').prepend('Wow, más de lo que esperaba.');
+				if(howManyMails <= 10){
+					$('#quote1').prepend('Con que ' + howManyMails +' nada más eh? tal vez no debí mandarte esto a ti.');
+
+				} else if (howManyMails > 10 && howManyMails < 20){
+					$('#quote1').prepend('Claro, ya me imaginaba de 10 a 20, no está mal.');
+
+				} else if (howManyMails >= 20){
+					$('#quote1').prepend('Vaya! <span class="boldie">' + howManyMails +'!?</span> No mientas, me huele a <span class="boldie">bluff.</span>');
+
+				}
+				$('.firstQuote').remove();
+				$('.quotes:first-child').removeClass('hiddenQuote');
+			};
 		});
 	// END FIRST INTERACTION
 
 	// SET TIMER FOR LOADING
 		$('#quote1').click(function() {
-			
+			$('#quote2').removeClass('quotes');
+			setTimeout(function(){
+				$('#quote2').remove();
+				$('.quotes:first-child').removeClass('hiddenQuote');
+			},2000);
 		});
 	// END SET TIMER
 
